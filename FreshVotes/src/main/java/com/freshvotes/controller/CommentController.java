d5ad39168821e45e868307db585b913c6bde9362
Pooja_Dhage <63343850+PoojaDhage2@users.noreply.github.com>
2023-06-18 16:47:21 +0530

    Add files via upload

---
+++ FreshVotes/src/main/java/com/freshvotes/controller/CommentController.java
@@ -0,0 +1,72 @@
+package com.freshvotes.controller;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.freshvotes.domain.Comment;
+import com.freshvotes.domain.Feature;
+import com.freshvotes.domain.User;
+import com.freshvotes.repository.CommentRepository;
+import com.freshvotes.repository.FeatureRepository;
+
+@Controller
+@RequestMapping("/products/{productId}/features/{featureId}/comments")
+public class CommentController {
+
+	@Autowired
+	private CommentRepository commentRepo;
+	
+	@Autowired
+	private FeatureRepository featureRepo;
+	
+	@GetMapping("")
+	@ResponseBody
+	public List<Comment> getComments(@PathVariable Long featureId){
+		List<Comment> findByFeatureId = commentRepo.findByFeatureId(featureId);
+		return findByFeatureId;
+	}
+	
+	@PostMapping("")
+	public String postComment(@AuthenticationPrincipal User user, @PathVariable Long featureId, @PathVariable Long productId, Comment rootComment, 
+			@RequestParam(required = false) Long parentId, @RequestParam(required = false) String childCommentText) {
+
+		Optional<Feature> featureOpt = featureRepo.findById(featureId);
+		if(!StringUtils.isEmpty(rootComment.getText())) {
+			populateCommentMetadata(user, featureOpt, rootComment);
+			commentRepo.save(rootComment);
+		}else if(parentId!=null) {
+			Comment comment=new Comment(); 
+			Optional<Comment> parentCommentOpt = commentRepo.findById(parentId);
+			if(parentCommentOpt.isPresent()) {
+				comment.setComment(parentCommentOpt.get());
+			}
+			comment.setText(childCommentText);
+			
+			populateCommentMetadata(user, featureOpt, comment);
+			commentRepo.save(comment);
+		}
+		
+		return "redirect:/products/"+productId+"/features/"+featureId;
+	}
+
+	private void populateCommentMetadata(User user, Optional<Feature> featureOpt, Comment comment) {
+		if(featureOpt.isPresent()) {
+			comment.setFeature(featureOpt.get());
+		}
+		
+		comment.setUser(user);
+		comment.setCreatedDate(new Date());
+	}
+}
